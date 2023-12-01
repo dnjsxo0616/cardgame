435f016ffa60b5b344aec24de0ba8bc15019ce1a
KDT live <dnjsxo0616@gmail.com>
2023-12-02 00:11:39 +0900

    feat: 게임판 출력 및 선택된 숫자 표시 기능 추가

--- src/main/java/view/output/OutputView.java
+++ src/main/java/view/output/OutputView.java
@@ -22,4 +22,32 @@ public class OutputView {
     public static void println(String message) {
         System.out.println(message);
     }
+
+    public static void printBackOfBoard(int[][] cardBoard) {
+        for (int i = 0; i < cardBoard.length; i++) {
+            for (int j = 0; j < cardBoard[i].length; j++) {
+                if (cardBoard[i][j] == 0) {
+                    System.out.print(" ");
+                } else {
+                    System.out.print("X");
+                }
+                System.out.print(" ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void printSelectNumberBoard(int[][] cardBoard, int[] input1, int[] input2) {
+        for (int i = 0; i < cardBoard.length; i++) {
+            for (int j = 0; j < cardBoard[i].length; j++) {
+                if (i == input1[0] && j == input1[1] || i == input2[0] && j == input2[1]) {
+                    System.out.print(cardBoard[i][j]);
+                } else {
+                    System.out.print("X");
+                }
+                System.out.print(" ");
+            }
+            System.out.println();
+        }
+    }
 }
