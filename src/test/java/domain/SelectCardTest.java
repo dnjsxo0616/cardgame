f409484634794cab4e475a2936a69f008fc950ce
KDT live <dnjsxo0616@gmail.com>
2023-12-01 19:38:59 +0900

    test: 카드 선택 테스트

--- src/test/java/domain/SelectCardTest.java
+++ src/test/java/domain/SelectCardTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
+import static org.junit.jupiter.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class SelectCardTest {
@@ -26,5 +27,19 @@ class SelectCardTest {
         for (int i = 0; i < 3; i++) {
             assertEquals(6, cardBoard[i].length);
         }
+    }
+
+    @Test
+    @DisplayName("카드_선택_테스트")
+    void testSelect() {
+        List<Integer> deck = ShuffleDeck.createShuffleDeck();
+        int[][] cardBoard = SelectCard.selectCard(deck);
+
+        for (int i = 0; i < SelectCard.CARD_BOARD_ROW; i++) {
+            for (int j = 0; j < SelectCard.CARD_BOARD_COL; j++) {
+                assertTrue(cardBoard[i][j] >= 1 && cardBoard[i][j] <= 8);
+            }
+        }
+        assertEquals(6, deck.size());
     }
 }
