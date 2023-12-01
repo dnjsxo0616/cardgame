c669e5b602caff8eefac09f029b6d9dc6707e966
KDT live <dnjsxo0616@gmail.com>
2023-12-01 19:47:53 +0900

    feat: 남은 카드를 세는 기능 추가

--- src/main/java/domain/Board.java
+++ src/main/java/domain/Board.java
@@ -10,4 +10,16 @@ public class Board {
     public int[][] getCardBoard() {
         return cardBoard;
     }
+
+    public int cardBoardSize() {
+        int count = 0;
+        for (int i = 0; i < cardBoard.length; i++) {
+            for (int j = 0; j < cardBoard[i].length; j++) {
+                if (cardBoard[i][j] != 0) {
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
 }
