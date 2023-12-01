823742e0504bb48bc45f8ff99d91af48db1a16c7
KDT live <dnjsxo0616@gmail.com>
2023-12-02 00:19:34 +0900

    feat: 문자열 배열을 정수 배열로 변환하는 기능 추가

--- src/main/java/domain/Parse.java
+++ src/main/java/domain/Parse.java
@@ -4,4 +4,12 @@ public class Parse {
     public static String[] parseComma(String input) {
         return input.replaceAll("[()\\s]", "").split(",");
     }
+
+    public static int[] covertToInt(String[] input) {
+        try {
+            return new int[]{Integer.parseInt(input[0]) - 1, Integer.parseInt(input[1]) - 1};
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("입력된 좌표를 정수로 변환할 수 없습니다.");
+        }
+    }
 }
