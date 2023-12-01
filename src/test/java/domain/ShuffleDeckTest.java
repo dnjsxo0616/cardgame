d3f13c884a43ad98ea8dfd80e95cb46a05d5fb18
KDT live <dnjsxo0616@gmail.com>
2023-12-01 19:00:19 +0900

    test: 각 숫자의 개수 테스트

--- src/test/java/domain/ShuffleDeckTest.java
+++ src/test/java/domain/ShuffleDeckTest.java
@@ -3,7 +3,10 @@ package domain;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.Collections;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -11,9 +14,19 @@ class ShuffleDeckTest {
 
     @Test
     @DisplayName("카드_크기_테스트")
-    void createShuffleDeck() {
+    void deckSize() {
         List<Integer> deck = ShuffleDeck.createShuffleDeck();
 
         assertEquals(24, deck.size());
+    }
+
+    @Test
+    @DisplayName("각_3장_여부_테스트")
+    void cardNumber() {
+        List<Integer> deck = ShuffleDeck.createShuffleDeck();
+
+        for(int i=1; i<=8; i++) {
+            assertEquals(3, Collections.frequency(deck, i));
+        }
     }
 }
