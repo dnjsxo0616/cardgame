47bcd3e97a825a6eee752e12d1fe08df067430a7
KDT live <dnjsxo0616@gmail.com>
2023-12-01 19:31:59 +0900

    test: 카드 열 테스트

--- src/test/java/domain/SelectCardTest.java
+++ src/test/java/domain/SelectCardTest.java
@@ -5,7 +5,7 @@ import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class SelectCardTest {
     @Test
@@ -15,5 +15,16 @@ class SelectCardTest {
         int[][] cardBoard = SelectCard.selectCard(deck);
 
         assertEquals(3, cardBoard.length);
+    }
+
+    @Test
+    @DisplayName("카드_열_테스트")
+    void testCol() {
+        List<Integer> deck = ShuffleDeck.createShuffleDeck();
+        int[][] cardBoard = SelectCard.selectCard(deck);
+
+        for (int i = 0; i < 3; i++) {
+            assertEquals(6, cardBoard[i].length);
+        }
     }
 }
