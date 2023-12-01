b6e254ba03bf23667f1e81671ec5f203a2f1c0f7
KDT live <dnjsxo0616@gmail.com>
2023-12-02 00:27:10 +0900

    test: covertToInt 테스트

--- src/test/java/domain/ParseTest.java
+++ src/test/java/domain/ParseTest.java
@@ -1,5 +1,6 @@
 package domain;
 
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -14,5 +15,24 @@ class ParseTest {
         String[] expected = {"1", "2"};
 
         assertArrayEquals(expected, Parse.parseComma(input));
+    }
+
+    @Test
+    @DisplayName("정수_변환_테스트")
+    void covertToInt() {
+        String[] input = {"1", "2"};
+        int[] expected = {0, 1};
+
+        assertArrayEquals(expected, Parse.covertToInt(input));
+    }
+
+    @Test
+    @DisplayName("정수_변환_오류_테스트")
+    void covertToIntFail() {
+        String[] input = {"a", "n"};
+
+        Assertions.assertThatThrownBy(() -> Parse.covertToInt(input))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("입력된 좌표를 정수로 변환할 수 없습니다.");
     }
 }
