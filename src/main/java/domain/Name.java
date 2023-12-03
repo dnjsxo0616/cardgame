a0d2504a46ac522739b44d36cdcb3204ca6f8396
KDT live <dnjsxo0616@gmail.com>
2023-12-03 21:57:51 +0900

    feat: 빈 문자열 유효성 검사 기능 추가

--- src/main/java/domain/Name.java
+++ src/main/java/domain/Name.java
@@ -6,6 +6,7 @@ public class Name {
     private final String name;
 
     public Name(final String name) {
+        validateEmpty(name);
         validateLength(name);
         this.name = name;
     }
@@ -13,6 +14,12 @@ public class Name {
     private static void validateLength(final String input) {
         if (input.length() > NAME_MAX_LENGTH) {
             throw new IllegalArgumentException("이름은 최대 4자입니다.");
+        }
+    }
+
+    private static void validateEmpty(final String input) {
+        if (input.isEmpty()) {
+            throw new IllegalArgumentException("이름을 입력해주세요.");
         }
     }
 
