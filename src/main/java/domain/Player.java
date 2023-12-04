73973df6dc274592a5743f10123a53653e06a0f4
KDT live <dnjsxo0616@gmail.com>
2023-12-04 15:37:24 +0900

    feat: 점수 더하기 기능 추가
    - plusScore로 주어진 횟수에 따라 플레이어의 점수를 증가시킴
    - PLUS_NUMBER와 count를 곱한 값을 계산하는 multiplicationPlusNumber 메서드 정의

--- src/main/java/domain/Player.java
+++ src/main/java/domain/Player.java
@@ -1,6 +1,7 @@
 package domain;
 
 public class Player {
+    private static final int PLUS_NUMBER = 10;
     private static final int START_NUMBER = 0;
 
     private final Name name;
@@ -9,5 +10,21 @@ public class Player {
     private Player(final String name) {
         this.name = Name.createName(name);
         this.score = START_NUMBER;
+    }
+
+    private int multiplicationPlusNumber(int count) {
+        return PLUS_NUMBER * count;
+    }
+
+    public void plusScore(int count) {
+        score += multiplicationPlusNumber(count);
+    }
+
+    public Name getName() {
+        return name;
+    }
+
+    public int getScore() {
+        return score;
     }
 }
