905b16e455d6de62758bf15cb5eed234d9907f0a
KDT live <dnjsxo0616@gmail.com>
2023-12-04 15:21:09 +0900

    feat: 이름 파싱 기능 추가

--- src/main/java/domain/Parse.java
+++ src/main/java/domain/Parse.java
@@ -1,8 +1,16 @@
 package domain;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class Parse {
+    private static final String COMMA = ",";
+    private static final String INPUT_FORMAT = "[()\\s]";
+    private static final String REPLACE_STRING = "";
+
     public static String[] parseComma(String input) {
-        return input.replaceAll("[()\\s]", "").split(",");
+        return input.replaceAll(INPUT_FORMAT, REPLACE_STRING).split(COMMA);
     }
 
     public static int[] covertToInt(String[] input) {
@@ -11,5 +19,14 @@ public class Parse {
         } catch (NumberFormatException e) {
             throw new IllegalArgumentException("입력된 좌표를 정수로 변환할 수 없습니다.");
         }
+    }
+
+    public static List<String> parseName(String input) {
+        String[] strNames = input.split(COMMA);
+        List<String> names = new ArrayList<>();
+        for (String name : strNames) {
+            names.add(name.trim());
+        }
+        return names;
     }
 }
