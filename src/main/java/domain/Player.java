76cd6a6d9ca2c966c2369878b69aba78e6217a0f
KDT live <dnjsxo0616@gmail.com>
2023-12-04 15:41:19 +0900

    feat: 플레이어 점수 비교 기능 추가

--- src/main/java/domain/Player.java
+++ src/main/java/domain/Player.java
@@ -24,6 +24,18 @@ public class Player {
         score += multiplicationPlusNumber(count);
     }
 
+    public Name scoreComparison(Player player) {
+        if (this.score > player.score) {
+            return this.name;
+        }
+
+        if (this.score < player.score) {
+            return player.name;
+        }
+
+        return null;
+    }
+
     public Name getName() {
         return name;
     }
