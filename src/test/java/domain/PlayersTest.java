edfdc9b28e44bfcc291e56e932514d853995e6f2
KDT live <dnjsxo0616@gmail.com>
2023-12-04 17:35:05 +0900

    test: 우승자 찾기 테스트

--- src/test/java/domain/PlayersTest.java
+++ src/test/java/domain/PlayersTest.java
@@ -3,6 +3,9 @@ package domain;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class PlayersTest {
@@ -15,5 +18,20 @@ class PlayersTest {
         assertEquals("Alex", players.getPlayers(0).getName().toString());
         assertEquals("Han", players.getPlayers(1).getName().toString());
         assertEquals("Kim", players.getPlayers(2).getName().toString());
+    }
+
+    @Test
+    @DisplayName("우승자_찾기_테스트")
+    void winnerPlayer() {
+        String input = "Alex, Han, Kim";
+        Players players = Players.createNewPlayers(input);
+
+        players.getPlayers(0).plusScore(3);
+        players.getPlayers(1).plusScore(2);
+        players.getPlayers(2).plusScore(1);
+
+        List<String> winnerNames = players.winnerPlayer();
+
+        assertEquals(List.of("Alex"), winnerNames);
     }
 }
