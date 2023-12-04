3962ca476271f7555362560cc3513b3c36c7826a
KDT live <dnjsxo0616@gmail.com>
2023-12-05 03:25:57 +0900

    refactor: Controller 기능 추가 및 수정

--- src/main/java/controller/Controller.java
+++ src/main/java/controller/Controller.java
@@ -2,21 +2,29 @@ package controller;
 
 import domain.Board;
 import domain.BoardPosition;
-import domain.ShuffleDeck;
+import domain.Players;
 import view.contents.ContentsMessage;
 import view.input.InputView;
 import view.output.OutputView;
 
-import java.util.List;
-
 public class Controller {
-    public void play() {
-        List<Integer> deck = ShuffleDeck.createShuffleDeck();
-        List<Integer> pairsDeck = ShuffleDeck.createPairsDeck(deck);
-        System.out.println(pairsDeck);
+    private Players players;
+    private int playerIndex = 0;
+    private int multipleCount = 1;
 
+    public void play() {
         Board board = new Board();
-       OutputView.printBoard(board.getCardBoard());
+//        OutputView.printBoard(board.getCardBoard());
+
+        while (true) {
+            try {
+                OutputView.print(ContentsMessage.PLAYERS_MESSAGE);
+                players = Players.createNewPlayers(InputView.readInput());
+                break;
+            } catch (IllegalArgumentException e) {
+                OutputView.println(e.getMessage());
+            }
+        }
 
         int roundCount = 0;
         while (!board.checkBoard()) {
@@ -24,6 +32,7 @@ public class Controller {
             OutputView.printNewLine();
             OutputView.printBackOfBoard(board.getCardBoard());
             OutputView.println(ContentsMessage.INPUT_PLAY_MESSAGE, roundCount, board.cardBoardSize());
+            OutputView.printPlayerScore(players);
 
             int[] input1;
             int[] input2;
@@ -48,12 +57,21 @@ public class Controller {
                 board.removeNumber(input1, input2);
                 OutputView.printBackOfBoard(board.getCardBoard());
                 OutputView.printNewLine();
+
+                players.getPlayer(playerIndex).plusScore(multipleCount);
+                multipleCount++;
+            } else {
+                playerIndex = (playerIndex + 1) % players.getPlayersSize();
+                multipleCount = 1;
             }
         }
 
         OutputView.print(ContentsMessage.FINISH_MESSAGE);
         OutputView.printNewLine();
         OutputView.print(ContentsMessage.CLOSE_MESSAGE);
+        OutputView.printNewLine();
+
+        OutputView.printWinnerPlayer(players);
 
     }
 
