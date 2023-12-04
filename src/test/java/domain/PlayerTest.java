f13f266b6dfd0345cb09c90f7b9298a511d1b74d
KDT live <dnjsxo0616@gmail.com>
2023-12-04 15:52:22 +0900

    test: 점수 더하기 테스트

--- src/test/java/domain/PlayerTest.java
+++ src/test/java/domain/PlayerTest.java
@@ -14,4 +14,13 @@ class PlayerTest {
         assertEquals("Han", player.getName().toString());
         assertEquals(0, player.getScore());
     }
+
+    @Test
+    @DisplayName("점수_더하기_테스트")
+    void plusScore() {
+        Player player = Player.createPlayer("Han");
+        player.plusScore(2);
+
+        assertEquals(20, player.getScore());
+    }
 }
