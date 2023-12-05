0445694f8dce97bee7d21daf575ba3066869ada8
KDT live <dnjsxo0616@gmail.com>
2023-12-05 13:00:48 +0900

    refactor: 중복되는 로직 메서드로 분리 리팩터링

--- src/main/java/controller/Controller.java
+++ src/main/java/controller/Controller.java
@@ -14,25 +14,12 @@ public class Controller {
 
     public void play() {
         Board board = new Board();
-//        OutputView.printBoard(board.getCardBoard());
-
-        while (true) {
-            try {
-                OutputView.print(ContentsMessage.PLAYERS_MESSAGE);
-                players = Players.createNewPlayers(InputView.readInput());
-                break;
-            } catch (IllegalArgumentException e) {
-                OutputView.println(e.getMessage());
-            }
-        }
+        initStart();
 
         int roundCount = 0;
         while (!board.checkBoard()) {
             roundCount++;
-            OutputView.printNewLine();
-            OutputView.printBackOfBoard(board.getCardBoard());
-            OutputView.println(ContentsMessage.INPUT_PLAY_MESSAGE, roundCount, board.cardBoardSize());
-            OutputView.printPlayerScore(players);
+            printHeadOutput(board, roundCount);
 
             int[] input1;
             int[] input2;
@@ -41,9 +28,7 @@ public class Controller {
                 try {
                     input1 = getInput(1);
                     input2 = getInput(2);
-                    if (board.checkEmptySpace(input1, input2)) {
-                        throw new IllegalArgumentException("빈 공간의 좌표를 입력하셨습니다.");
-                    }
+                    checkEmptySpace(board, input1, input2);
                     break;
                 } catch (IllegalArgumentException e) {
                     OutputView.println(e.getMessage());
@@ -51,28 +36,15 @@ public class Controller {
                 OutputView.printNewLine();
             }
 
-            OutputView.printSelectNumberBoard(board.getCardBoard(), input1, input2);
+            printSelectBoard(board, input1, input2);
 
             if (board.checkEqualNumber(input1, input2)) {
-                board.removeNumber(input1, input2);
-                OutputView.printBackOfBoard(board.getCardBoard());
-                OutputView.printNewLine();
-
-                players.getPlayer(playerIndex).plusScore(multipleCount);
-                multipleCount++;
+                removeAndScorePlus(board, input1, input2);
             } else {
-                playerIndex = (playerIndex + 1) % players.getPlayersSize();
-                multipleCount = 1;
+                changePlayer();
             }
         }
-
-        OutputView.print(ContentsMessage.FINISH_MESSAGE);
-        OutputView.printNewLine();
-        OutputView.print(ContentsMessage.CLOSE_MESSAGE);
-        OutputView.printNewLine();
-
-        OutputView.printWinnerPlayer(players);
-
+        printFinishOutput();
     }
 
     private static int[] getInput(int inputCount) {
@@ -84,5 +56,63 @@ public class Controller {
                 OutputView.println(e.getMessage());
             }
         }
+    }
+
+    private void printHeadOutput(Board board, int roundCount) {
+        OutputView.println(ContentsMessage.CURRENT_BOARD.getMessage());
+        OutputView.printBackOfBoard(board.getCardBoard());
+        OutputView.printNewLine();
+
+        OutputView.println(ContentsMessage.INPUT_PLAY_MESSAGE, roundCount, board.cardBoardSize());
+
+        OutputView.printWhoPlayer(ContentsMessage.WHO_PLAYER_MESSAGE, players.getPlayer(playerIndex).getName().toString());
+        OutputView.printPlayerScore(players);
+    }
+
+    private void printFinishOutput() {
+        OutputView.print(ContentsMessage.FINISH_MESSAGE);
+        OutputView.printNewLine();
+        OutputView.print(ContentsMessage.CLOSE_MESSAGE);
+        OutputView.printNewLine();
+
+        OutputView.printWinnerPlayer(players);
+    }
+
+    private void removeAndScorePlus(Board board, int[] input1, int[] input2) {
+        board.removeNumber(input1, input2);
+        OutputView.printBackOfBoard(board.getCardBoard());
+        OutputView.printNewLine();
+
+        players.getPlayer(playerIndex).plusScore(multipleCount);
+        multipleCount++;
+    }
+
+    private void changePlayer() {
+        playerIndex = (playerIndex + 1) % players.getPlayersSize();
+        multipleCount = 1;
+    }
+
+    private void initStart() {
+        while (true) {
+            try {
+                OutputView.print(ContentsMessage.PLAYERS_MESSAGE);
+                players = Players.createNewPlayers(InputView.readInput());
+                break;
+            } catch (IllegalArgumentException e) {
+                OutputView.println(e.getMessage());
+            }
+        }
+    }
+
+    private void checkEmptySpace(Board board, int[] input1, int[] input2) {
+        if (board.checkEmptySpace(input1, input2)) {
+            throw new IllegalArgumentException("빈 공간의 좌표를 입력하셨습니다.");
+        }
+    }
+
+    private void printSelectBoard(Board board, int[] input1, int[] input2) {
+        OutputView.printNewLine();
+        OutputView.println(ContentsMessage.SELECT_BOARD.getMessage());
+        OutputView.printSelectNumberBoard(board.getCardBoard(), input1, input2);
     }
 }
